17800bfa6773d510cc08f4294fd014ecd6692fd1
ax2543sn-s <ax2543sn-s@student.lu.se>
2020-12-03 17:47:38 +0100

    k

--- project_2/main.m
+++ project_2/main.m
@@ -6,7 +6,8 @@ spde.G2 = spde.G*spde.G;
 I_obs = reshape(sum(A,1),sz);
 I_grid = reshape(sum(A_grid,1),sz);
 q_beta = 1e-6;
-
+%% Specify B 
+B;
 %% Creaate X-mode
 global x_mode;
 x_mode = [];
@@ -14,43 +15,45 @@ par = fminsearch( @(theta) GMRF_negloglike(theta, Y, A, B, spde), [0 0]);
 E_xy = x_mode;
 
 %%
-N = size(B,2);
+n_B = size(B,2);
 tau = exp(par(1));
 kappa2 = exp(par(2));
 
+% CAR(1)
 Q_x = tau*(kappa2*spde.C + spde.G);
-qbeta=1e-6;
-Qbeta = qbeta*speye(N);
-Nbeta = N;
-Qall = blkdiag(Q_x,Qbeta);
+Qbeta = q_beta*speye(n_B);
+% 
+Qtilde = blkdiag(Q_x,Qbeta);
 Atilde = [A B];
-[~, ~, Q_xy] = GMRF_taylor(E_xy, Y, Atilde, Qall);
-%% TEST
-E_xy = A_grid * x_mode(1:end - Nbeta);
-E_beta = x_mode(end - Nbeta + 1:end);
+[~, ~, Q_xy] = GMRF_taylor(E_xy, Y, Atilde, Qtilde);
+%% Simulation
+E_xy = A_grid * x_mode(1:end - n_B);
+E_beta = x_mode(end - n_B + 1:end);
 E_Bbeta = B_grid * E_beta;
 E_zy = E_xy + E_Bbeta;
 E_out = exp(E_zy);
 
 %reuse taylor expansion to compute posterior precision
 
-e = [zeros(size(Q_xy,1)-Nbeta, Nbeta); eye(Nbeta)];
+e = [zeros(size(Q_xy,1)-n_B, n_B); eye(n_B)];
 V_beta0 = e'*(Q_xy\e);
 E_beta;
 beta_int_size = sqrt(diag(V_beta0)) * 1.96;
-for i = 1:Nbeta
+for i = 1:n_B
   fprintf(1, 'Beta %d: %11.4f +- %.4f\n', i, E_beta(i), beta_int_size(i));
 end
 
+%Simulation over 1000 samples of the approximate posterior
 
-
-%1000 samples from the approximate posterior
 Rxy = chol(Q_xy);
-x_samp = bsxfun(@plus, x_mode, Rxy \ randn(size(Rxy,1), 1000));
-meanx_samp = mean(x_samp(1:end-Nbeta,:));
+x_sample = repmat(x_mode(:),[1,1000]) + Rxy\randn(size(Rxy,1),1000);
+
+%% Test
+%Rxy \ randn(size(Rxy,1), 1000));
+meanx_samp = mean(x_samp(1:end-n_B,:));
 z_samp = Atilde*x_samp;
 Vzy = var(z_samp,0,2);
-Vx  = 1 / (size(x_samp,2) - 1) * sum(x_samp(1:end-Nbeta,:).^2,2);
+Vx  = 1 / (size(x_samp,2) - 1) * sum(x_samp(1:end-n_B,:).^2,2);
 Vbeta = sum((B_grid * V_beta0) .* B_grid, 2);
 % Vzy = Vx + Vbeta;
 Vout = exp(Vzy);
