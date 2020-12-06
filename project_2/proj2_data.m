7ac1780eb0207e4d6a426d70f39beffd6aec2009
ax2543sn-s <ax2543sn-s@student.lu.se>
2020-12-06 18:40:34 +0100

    Uppdaterad för korrekt ordning

--- project_2/proj2_data.m
+++ project_2/proj2_data.m
@@ -1,6 +1,9 @@
 %% load data
 load( 'AL_data_2000_BP.mat' )
-
+% B vector containing standard data on human land use
+B = [B(:,1)  B(:,2)  B(:,3)  B(:,4) B(:,5) B(:,6)];
+% B vector with logitransformed human land use
+%B = [B(:,1)  B(:,2)  B(:,3)  B(:,4) B(:,5) B(:,7)];
 %% create precision matrix
 spde.C = speye(prod(sz));
 spde.G = igmrfprec(sz,1);
@@ -49,6 +52,7 @@ x_mode = [];
 % [0 0] - two parameters to estimate, lets start with 0 in log-scale.
 
 par = fminsearch( @(theta) GMRF_negloglike(theta, Y, A, B, spde), [0 0]);
+%par2 = fminsearch( @(theta) GMRF_negloglike(theta, Y, A, B2, spde, alpha), [0 0]);
 %conditional mean is now given be the mode
 E_xy = x_mode;
 
@@ -67,8 +71,27 @@ Qall = blkdiag(Q_x,Qbeta);
 Atilde = [A B];
 
 [~, ~, Q_xy] = GMRF_taylor(E_xy, Y, Atilde, Qall);
-%%
-e = [zeros(size(Q_xy,1)-size(B,2), size(B,2)); eye(size(B,2))];
+%% Calculation of relevant Betas
+e = [zeros(size(Q_xy,1)-N, N); eye(N)];
 V_beta0 = e'*(Q_xy\e);
+beta_CI95 =  sqrt(diag(V_beta0)) * 1.96;
+E_beta = x_mode(end - N + 1:end);
 
+for i = 1:N
+ print = ['Beta ', num2str(i), ' : ', num2str(E_beta(i)), ' +- ', num2str(beta_CI95(i))];
+ disp(print);
+end
+
+%CI of beta6 is further from zero -> B(1:6) is more appropriate
+
+%CI_U = E_beta(2:end) + beta_i(2:end);
+%CI_L = E_beta(2:end) - beta_i(2:end);
+
+%Plotted using matlab r2014b, broken on newer versions.
+%figure(6);
+%candle(CI_U,CI_U,CI_L,CI_L 'b');
+%xlim([0.5,5.5]);
+%line([0 7],[0 0],'LineWidth',2);
+%ylim([-2 2]);
+%grid on;
 
