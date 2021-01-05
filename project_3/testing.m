bea3deaf7bfb2ac5e98b43f872cb04f9a0c0fcc9
ax2543sn-s <ax2543sn-s@student.lu.se>
2021-01-05 11:33:46 +0100

    Final revision

--- project_3/testing.m
+++ project_3/testing.m
@@ -9,62 +9,56 @@ beta = X\colstack(img)';
 beta = reshape(beta', sz(1), sz(2), []);
 %and treat the beta:s as the image
 %Perform a PCA on the regression coefficients to find important components
-[y_beta, ~, P_beta] = pca(colstack(beta)); % PCA med alla regressionskoefficienter?
+[y_beta, ~, P_beta] = pca(colstack(beta(:,:,3:end))); % PCA med alla regressionskoefficienter?
 y_beta = reshape(y_beta, sz(1), sz(2), []);
-%% PCA on regression
-xc = colstack(y_beta(:,:,3:11));
-
-[cl2,theta] = kmeans(xc, nbr_class, inf, 0);
+%% KMEANS on regression
+xc_beta = colstack(y_beta(:,:,1:end));
+[cl_beta,theta_beta] = kmeans(xc_beta, nbr_class, inf, 0);
 figure, 
-imagesc(reshape(cl2, 87, 102))
+imagesc(reshape(cl_beta, 87, 102))
 
-%% PCA on timedata
-[y,V,P] = pca(colstack(img));
+%% KMEANS on timedata
+[y,~,P_time] = pca(colstack(img));
 y = reshape(y, sz(1), sz(2), []);
-xc_2 = colstack(y); 
-[cl_2,theta] = kmeans(xc_2, nbr_class, inf, 0);
+xc_time = colstack(y); 
+[cl_time,theta_time] = kmeans(xc_time, nbr_class, inf, 0);
 figure, 
-imagesc(reshape(cl_2, 87, 102))
-
+imagesc(reshape(cl_time, 87, 102))
+figure, 
+imagesc(beta(:,:,1));
 %% GMM on Regression model
-[theta,prior]=normmix_gibbs(xc,nbr_class);
-%[theta,prior]=normmix_gibbs(xc,nbr_class,[200, 100],0);
-[cl,cl_ind,p]=normmix_classify(xc,theta,prior);
-%y=reshape(p,[87, 102 ,nbr_class]);
-%figure,
-%image(rgbimage(y))
+[theta_beta,prior]=normmix_gibbs(xc_beta,nbr_class);
+[cl,cl_ind,~]=normmix_classify(xc_beta,theta_beta,prior);
 figure,
 imagesc(reshape(cl, sz(1:2)));
 %% creating alpha posteriors from GMM
-
 alpha_posterior = log(prior(2:nbr_class)/prior(1));
-cl_img= reshape(cl_ind,[sz(1:2),nbr_class ]);
+cl_img = reshape(cl_ind,[sz(1:2),nbr_class ]);
 
 %% Simulating MRF
 N1 = [0 1 0; 1 0 1; 0 1 0]; 
 N2 = [1 1 1; 1 0 1; 1 1 1]; 
 N3 = [1 0 1; 0 0 0; 1 0 1];
 
-y_m = y_beta(:,:,3:11);
+y_m = y_beta(:,:,1:end);
 y_c = colstack(y_m);
 iter = 1000;
 Burnin = 350;
 beta = 0; 
+% Suggested prior
 beta_prior = 1/10;
 
 %Plog = zeros(sz(1),sz(2),nbr_class);
 Plog = zeros(length(iter),1);
-
 z_z = cl_img;
-
 z_sum = zeros(size(z_z));
 for i=1:iter
-    alpha_beta_post=mrf_gaussian_post([0 alpha_posterior],theta,y_m);
-    z_z= mrf_sim(z_z, N1, alpha_beta_post, beta, 1);
-    [~,Mz_b]=mrf_sim(z_z,N1,alpha_beta_post,beta,0);
+    alpha_beta_post=mrf_gaussian_post([0 alpha_posterior],theta_beta,y_m);
+    z_z= mrf_sim(z_z, N2, alpha_beta_post, beta, 1);
+    [~,Mz_b]=mrf_sim(z_z,N2,alpha_beta_post,beta,0);
     Plog(i) = sum(log(Mz_b(logical(z_z))));
     
-    [alpha_posterior, beta, acc_beta] = gibbs_alpha_beta(alpha_posterior, beta, z_z, N1, beta_prior);
+    [alpha_posterior, beta, acc_beta] = gibbs_alpha_beta(alpha_posterior, beta, z_z, N2, beta_prior);
 for k = 1:nbr_class
         ind_beta = find(z_z(:,:,k));
         [theta_beta{k}.mu, theta_beta{k}.Sigma] = gibbs_mu_sigma(y_c(ind_beta,:));
@@ -72,7 +66,7 @@ for k = 1:nbr_class
 end
  
  figure(6)
- image(rgbimage(z_sum))
+ image(rgbimage(z_z))
  title('MRK-sim, Beta ')
  drawnow    
      if i > Burnin   
